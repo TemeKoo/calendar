c852270245cef8ce3782db326ca0fa381a66f856
Teemu Koski <80845642+TemeKoo@users.noreply.github.com>
2021-04-18 18:14:59 +0300

    Create first version of calendar

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import date
+from calendar import Calendar, day_name, month_name
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world!"
+    calendar = Calendar()
+    day = date.today()
+    weeks = list(calendar.monthdays2calendar(day.year, day.month))
+    
+    while len(weeks) < 6:
+        weeks.append([(0, i) for i in range(7)])
+    
+    names = {}
+
+    names["weekdays"] = list(day_name)
+    names["month"] = month_name[day.month]
+
+    return render_template("index.html", weeks=weeks, names=names)
